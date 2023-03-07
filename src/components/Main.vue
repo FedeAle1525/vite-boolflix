35e5ae1d55240ae72a02c04b35753afb9bd0f159
FedeAle1525 <fedeale.1525@gmail.com>
2023-03-07 16:16:07 +0100

    VUE create Server Request

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,14 +1,46 @@
 <script>
+import axios from 'axios';
+import store from '../store';
 import CardFilm from './CardFilm.vue';
 
 export default {
 
   components: {
     CardFilm
+  },
+
+  data() {
+    return {
+      store
+    }
+  },
+
+  methods: {
+    searchFilms() {
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=088f7fa18f717fff2fc5bf18e9e57aab', {
+        params: {
+          api_key: store.apiKey,
+          language: store.language,
+          query: store.queryName,
+          page: store.page,
+          include_adults: store.adults
+        }
+      })
+        .then((resp) => {
+          this.store.films = resp.data.results;
+          console.log(resp.data.results);
+        })
+    }
+  },
+
+  mounted() {
+    this.searchFilms();
   }
+
 }
 </script>
 
+<!-- Parte HTML -->
 <template>
   <main>
     <div class="container">
@@ -19,6 +51,7 @@ export default {
   </main>
 </template>
 
+<!-- Parte CSS -->
 <style lang="scss" scoped>
 main {
   padding: 20px 0;
--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ import { reactive } from 'vue';
 const store = reactive({
 
   films: [],
-  queryName: '',
+  queryName: 'anelli',
   apiKey: '088f7fa18f717fff2fc5bf18e9e57aab',
   language: 'it-IT',
   page: 1,
