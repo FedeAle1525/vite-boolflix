a275208f3b00cb119d1a5bf6f87acfd3ccfc86fd
FedeAle1525 <fedeale.1525@gmail.com>
2023-03-08 14:30:53 +0100

    VUE create and use component Language

--- src/components/CardFilm.vue
+++ src/components/CardFilm.vue
@@ -1,4 +1,6 @@
 <script>
+import Language from './Language.vue'
+
 export default {
 
   props: {
@@ -6,6 +8,10 @@ export default {
       type: Object,
       required: true
     }
+  },
+
+  components: {
+    Language
   }
 }
 </script>
@@ -20,26 +26,9 @@ export default {
       <span>Titolo Originale:</span>
       "{{ film.original_title }}"
     </span>
-    <span class="language" v-if="film.original_language === 'en'">
-      <span>Lingua:</span>
-      <img src="/images/gb.png" alt="">
-    </span>
-    <span class="language" v-else-if="film.original_language === 'es'">
-      <span>Lingua:</span>
-      <img src="/images/es.png" alt="">
-    </span>
-    <span class="language" v-else-if="film.original_language === 'it'">
-      <span>Lingua:</span>
-      <img src=" /images/it.png" alt="">
-    </span>
-    <span class="language" v-else-if="film.original_language === 'ja'">
-      <span>Lingua:</span>
-      <img src="/images/jp.png" alt="">
-    </span>
-    <span class="language" v-else>
-      <span>Lingua:</span>
-      {{ film.original_language }}
-    </span>
+
+    <Language :language="film.original_language" />
+
     <span class="vote">
       <span>Voto:</span>
       {{ film.vote_average }}
---
+++ src/components/Language.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  props: {
+    language: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<template>
+  <span class="language" v-if="language === 'en'">
+    <span>Lingua:</span>
+    <img src="/images/gb.png" alt="">
+  </span>
+  <span class="language" v-else-if="language === 'es'">
+    <span>Lingua:</span>
+    <img src="/images/es.png" alt="">
+  </span>
+  <span class="language" v-else-if="language === 'it'">
+    <span>Lingua:</span>
+    <img src=" /images/it.png" alt="">
+  </span>
+  <span class="language" v-else-if="language === 'ja'">
+    <span>Lingua:</span>
+    <img src="/images/jp.png" alt="">
+  </span>
+  <span class="language" v-else>
+    <span>Lingua:</span>
+    {{ language }}
+  </span>
+</template>
+
+<style lang="scss" scoped>
+.language>span {
+  color: aqua;
+  text-decoration: underline;
+}
+</style>
