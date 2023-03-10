52558e9a846e272da92ef30d7ed288246da01edd
FedeAle1525 <fedeale.1525@gmail.com>
2023-03-10 16:15:14 +0100

    FEAT add <select> for filters

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -16,6 +16,16 @@ export default {
     }
   },
 
+  computed: {
+    getGenresSeries() {
+      return this.store.genresSeries;
+    },
+
+    getGenresFilms() {
+      return this.store.genresFilms;
+    }
+  },
+
   watch: {
     searchName() {
       if (this.searchName === '') {
@@ -36,7 +46,31 @@ export default {
         <input type="text" placeholder="Ricerca per Nome" v-model="searchName" @keyup.enter="setQueryName()">
         <input type="button" value="Ricerca" @click="setQueryName()">
       </div>
+
+      <div class="filters">
+
+        <div class="serie-tv">
+          <span>Serie TV</span>
+          <select id="">
+            <option value="" hidden>Filtra per Genere</option>
+            <option value="none">Nessun Genere</option>
+            <option :value="genre.name" v-for="(genre, i) in getGenresSeries" :key="i"> {{ genre.name }}</option>
+          </select>
+        </div>
+
+        <div class="films">
+          <span>Film</span>
+          <select id="">
+            <option value="" hidden>Filtra per Genere</option>
+            <option value="none">Nessun Genere</option>
+            <option :value="genre.name" v-for="(genre, i) in getGenresFilms" :key="i"> {{ genre.name }}</option>
+          </select>
+        </div>
+
+      </div>
     </div>
+
+
   </header>
 </template>
 
@@ -58,9 +92,30 @@ header {
   align-items: center;
   gap: 15px;
 
+  select {
+    padding: 0 5px;
+  }
+
   input {
     line-height: 20px;
     padding: 0 5px;
+  }
+}
+
+.filters {
+  display: flex;
+  gap: 20px;
+
+  .serie-tv>span {
+    margin-right: 10px;
+    color: salmon;
+    font-weight: bold;
+  }
+
+  .films>span {
+    margin-right: 10px;
+    color: sandybrown;
+    font-weight: bold;
   }
 }
 </style>
